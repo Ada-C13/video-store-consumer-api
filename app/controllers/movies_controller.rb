b00617a3801c28c200e64e6e291350d9e0444540
Rosalyn <syrosalynyu@gmail.com>
2020-06-23 13:35:45 -0700

    added actions to the Movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,21 +2,51 @@ class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
 
   def index
-    if params[:query]
-      data = MovieWrapper.search(params[:query])
-    else
-      data = Movie.all
-    end
-
+    data = Movie.all
     render status: :ok, json: data
+  end
+
+  def search_input
+  end
+
+  def search
+    @movie = Movie.find_by(title: params[:title])
+    if !@movie
+      @data = MovieWrapper.search(params[:title])
+
+    else
+      render(
+        status: :ok,
+        json: @movie.as_json(
+          only: [:title, :overview, :release_date, :inventory],
+          methods: [:available_inventory]
+          )
+      )
+    end
   end
 
   # TODO: create a new movie instance in our rental library 
   def create
-    # step 1. to find the specific movie, we need to use the external_id from the params that we get from the React Movies.js
-    # https://developers.themoviedb.org/3/movies/get-movie-details
+    @movie = Movie.new(
+      title: params["title"],
+      overview: params["overview"],
+      release_date: params["release_date"],
+      image_url: self.image_url(params["poster_path"]),
+      external_id: params["id"],
+      inventory: 10
+    )
     
-    # setp 2. 
+    if @movie.save
+      render(
+        status: :ok,
+        json: @movie.as_json(
+          only: [:title, :overview, :release_date, :inventory],
+          methods: [:available_inventory]
+          )
+      )
+    else
+      render status: :bad_request, json: { errors: { title: ["Can't successfully create the movie with title #{params["title"]}"] } }
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homepages#index'
 
   resources :customers, only: [:index]
   
   # TODO: new to add the create action in the movie controller 
   # so that user can add a movie from the search results to the rental library
-  resources :movies, only: [:index, :show, :create], param: :title
-
+  resources :movies, only: [:show, :create], param: :title
+  get "/library", to: "movies#index", as: "library"
+  get "/input", to: "movies#search_input", as: "search_input"
+  get "/search", to: "movies#search", as: "search"
   post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
   post "/rentals/:title/return", to: "rentals#check_in", as: "check_in"
   get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
 
-  root 'movies#index'
+  
 
 end
