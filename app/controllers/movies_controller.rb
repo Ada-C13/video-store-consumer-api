92ae0a1b443a2fed48028cb8d3638708a995fe0a
cojenco <icathyouyang@gmail.com>
2020-06-24 00:46:27 -0700

    restore movies#create route and controller action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,30 @@
 class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
 
+  # def create
+  #   new_movie = MovieWrapper.construct_movie(params)
+  #   new_movie.inventory = 10
+
+  #   if new_movie.save
+  #     render status: :ok, json: {}
+  #   else
+  #     render status: :bad_request, json: { errors: new_movie.errors.messages }
+  #   end
+  # end
+
+  def create
+    new_movie = Movie.new(movie_params)
+    new_movie.inventory = 5
+
+    if new_movie.save
+      render status: :ok, json: {}
+      return
+    else
+      render status: :bad_request, json: { errors: new_movie.errors.messages }
+      return
+    end
+  end
+
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
@@ -28,5 +52,9 @@ class MoviesController < ApplicationController
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
+  end
+
+  def movie_params
+    params.permit(:title, :overview, :release_date, :image_url, :external_id)
   end
 end
