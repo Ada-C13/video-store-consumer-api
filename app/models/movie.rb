8382d69c96968ed3b977a82b4b8789c658701e6c
Jocelyn Wang <wangjoc@gmail.com>
2020-06-24 08:27:41 -0700

    added in validation to confirm that a movie can only be entered once

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
+  validates :title, uniqueness: true  
+
   def available_inventory
     self.inventory - Rental.where(movie: self, returned: false).length
   end
