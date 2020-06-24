2e1b5ef64dd07a9b8edcc1f7ed154b74ed73283b
vwhwang <vwhwang38@gmail.com>
2020-06-24 09:31:43 -0700

    added config on movie route and movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :require_movie, only: [:show]
+  before_action :require_movie, only: [:show, :create]
 
   def index
     if params[:query]
@@ -10,6 +10,24 @@ class MoviesController < ApplicationController
 
     render status: :ok, json: data
   end
+# new movie
+
+  def create
+    new_movie = Movie.new(external_id: params[:external_id], title: params[:title], inventory: 7,overview: params[:overview], release_date: params[:release_date], image_url: params[:image_url] )
+    if !Movie.find_by(external_id: new_movie.external_id)
+
+      if new_movie.save
+        render status: :ok, json: {}
+      else
+        render status: :bad_request, json: { errors: movie.errors.messages }
+      end
+
+    else   
+      render status: bad_request, json: {errors: "movie already in database"}
+    end 
+
+  end 
+
 
   def show
     render(
