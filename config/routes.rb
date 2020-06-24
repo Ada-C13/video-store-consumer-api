112f5340736f80ebf4302a9ac091ff6d6ad1624f
Jocelyn Wang <wangjoc@gmail.com>
2020-06-24 08:28:05 -0700

    added in routing for index rentals and params for customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :customers, only: [:index]
+  resources :customers, only: [:index], param: :name
 
   resources :movies, only: [:index, :create, :show], param: :title
 
+  resources :rentals, only: [:index]
   post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
   post "/rentals/:title/return", to: "rentals#check_in", as: "check_in"
   get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
