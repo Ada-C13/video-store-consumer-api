28658ebbfee9337f4d2ccece71459193a755527c
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-06-24 17:20:35 -0700

    Wrote the test for the create method and passed all the tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -75,4 +75,28 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
 
     end
   end
+
+  describe "create" do
+    before do
+      @valid_movie = {
+        title: "XXXXX",
+        overview: "XXXXXXXX",
+        release_date: "2017-01-11",
+        inventory: 10,
+        image_url: "https://lh3.googleusercontent.com/pw/ACtC-3dgueD28nFt8fbmnEVSWrDdgXdH4dy91CXoWO818YTNFQlfnH-GN1O9t3zX4UEOGH3cncMC2Ze9WfNm13ofTlzOV97WdprqYmUPbj5H0oTS7Qwi8QtAEV8RFNyrcCJy09V04GFZQySqt9yhxf2v37Cg=w401-h397-no?authuser=0",
+        external_id: 2342
+      }
+    end
+    it "create a new movie instance/object" do 
+      # post movies_path, params: @valid_movie
+
+      
+      expect{post movies_path, params: @valid_movie}.must_differ "Movie.count", 1
+      must_respond_with :success
+
+      last_movie = Movie.last
+      expect(last_movie.title).must_equal "XXXXX"
+      p last_movie
+    end
+  end
 end
