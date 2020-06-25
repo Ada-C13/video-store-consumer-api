e32a58157b0f9080d4d1c16aad80fb45ca8984ad
thenora <thenora@gmail.com>
2020-06-24 19:12:24 -0700

    added model validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
+  validates :title, presence: true
+  validates :external_id, presence: true, uniqueness: true
 
   def available_inventory
     self.inventory - Rental.where(movie: self, returned: false).length
