eb0282571f52f694a75f0d60fdb1c77df56693f9
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-26 16:40:04 -0700

    added model validation tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -6,7 +6,8 @@ class MovieTest < ActiveSupport::TestCase
       "title": "Hidden Figures",
       "overview": "Some text",
       "release_date": "1960-06-16",
-      "inventory": 8
+      "inventory": 8,
+      "external_id": 1233454,
     }
   }
 
@@ -76,6 +77,29 @@ class MovieTest < ActiveSupport::TestCase
       movie.reload
       after_ai = movie.available_inventory
       after_ai.must_equal before_ai + 1
+    end
+  end
+
+  describe 'validations' do
+    it 'is valid when all the required fields are provided' do
+      new_movie = Movie.new( 
+        title: 'Harry Potter 3',
+        overview: 'The best movie ever!',
+        release_date: 'Wed, 22 Jun 1960',
+        inventory: 10,
+        image_url: "some image",
+        external_id: 15482656,
+      )
+
+      expect(new_movie.valid?).must_equal true
+    end
+    
+    it 'fails validation when there is one or more required field is missing' do
+      @movie.external_id = nil
+    
+      expect(@movie.valid?).must_equal false
+      expect(@movie.errors.messages).must_include :external_id
+
     end
   end
 end
